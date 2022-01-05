6a2e7c62ab2c909c5f005b9aaa2a140418879417
ambiennt <ahook701@gmail.com>
2022-01-04 23:53:03 -0800

    allow extra items to be added from yaml

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 void dllenter() {}
 void dllexit() {}
 
+DEFAULT_SETTINGS(settings);
+
 THook(void, "?die@ServerPlayer@@UEAAXAEBVActorDamageSource@@@Z", ServerPlayer *player, ActorDamageSource *source) {
 
     auto* gr = &LocateService<Level>()->getGameRules();
@@ -12,6 +14,7 @@ THook(void, "?die@ServerPlayer@@UEAAXAEBVActorDamageSource@@@Z", ServerPlayer *p
 
     if (player->isPlayerInitialized() && !isKeepInventory) {
 
+        // check valid position bounds
         auto newPos = player->getPos();
         newPos.y -= 1.62f;
 
@@ -45,7 +48,7 @@ THook(void, "?die@ServerPlayer@@UEAAXAEBVActorDamageSource@@@Z", ServerPlayer *p
             default: break;
         }
 
-        //set double chest
+        // set double chest
         BlockPos bp;
         const auto region = player->mRegion;
         auto normalizedChestPos_1 = bp.getBlockPos(newPos);
@@ -58,11 +61,11 @@ THook(void, "?die@ServerPlayer@@UEAAXAEBVActorDamageSource@@@Z", ServerPlayer *p
         auto chestBlock_2 = region->getBlockEntity(normalizedChestPos_2);
         auto chestContainer = chestBlock_1->getContainer();
 
-        //clear curse of vanishing items and get inventory
+        // clear curse of vanishing items and get inventory
         player->clearVanishEnchantedItems();
         auto playerInventory = player->mInventory->inventory.get();
 
-        //copy player inventory to chest
+        // copy player inventory to chest
         const int playerArmorSlots = 4;
         for (int i = 0; i < playerArmorSlots; i++) {
             auto armorItem = player->getArmor((ArmorSlot) i);
@@ -85,6 +88,60 @@ THook(void, "?die@ServerPlayer@@UEAAXAEBVActorDamageSource@@@Z", ServerPlayer *p
         chestContainer->addItemToFirstEmptySlot(playerUIItem);
         player->setPlayerUIItem(PlayerUISlot::CursorSelected, ItemStack::EMPTY_ITEM);
 
+        // add extra items from yaml settings
+        if (settings.enableExtraItems && !settings.extraItems.empty()) {
+            
+            for (auto &it : settings.extraItems) {
+
+                CommandItem cmi;
+                cmi.mId = it.id;
+                ItemStack currentExtraItem;
+
+                // ensure valid ranges
+                it.count = std::clamp(it.count, 0, 32767);
+                it.aux = std::clamp(it.aux, 0, 32767);
+
+                // hack - so we can test for valid item ID and get its stack size outside of loop
+                cmi.createInstanceWithoutCommand(&currentExtraItem, 0, it.aux, false);
+
+                int maxStackSize = currentExtraItem.getMaxStackSize();
+                int countNew = std::min(it.count, maxStackSize * playerInventorySlots);
+
+                if (!currentExtraItem.isNull()) {
+
+                    while (countNew > 0) {
+
+                        int currentStack = std::min(maxStackSize, countNew);
+                        cmi.createInstanceWithoutCommand(&currentExtraItem, currentStack, it.aux, false);
+                        countNew -= currentStack;
+
+                        if (!it.customName.empty()) {
+                            currentExtraItem.setCustomName(it.customName);
+                        }
+
+                        if (!it.lore.empty()) {
+                            currentExtraItem.setCustomLore(it.lore);
+                        }
+
+                        if (!it.enchants.empty()) {
+
+                            for (auto &enchantList : it.enchants) {
+
+                                for (auto &enchant : enchantList) {
+
+                                    EnchantmentInstance instance;
+                                    instance.type  = (Enchant::Type) std::clamp(enchant.first, 0, 36); // more valid range checks
+                                    instance.level = std::clamp(enchant.second, -32768, 32767);
+                                    EnchantUtils::applyEnchant(currentExtraItem, instance, true);
+                                }
+                            }
+                        }
+                        chestContainer->addItemToFirstEmptySlot(currentExtraItem);
+                    }
+                }
+            }
+        }
+
         std::string chestName = player->mPlayerName + "'s Gravestone";
         chestBlock_1->setCustomName(chestName);
         chestBlock_2->setCustomName(chestName);
@@ -92,7 +149,7 @@ THook(void, "?die@ServerPlayer@@UEAAXAEBVActorDamageSource@@@Z", ServerPlayer *p
         ((ChestBlockActor*)chestBlock_1)->mNotifyPlayersOnChange = true;
         chestBlock_1->onChanged(*region);
 
-        //avoid unnecessary call of ServerPlayer::clearVanishEnchantedItems and Player::dropEquipment
+        // avoid unnecessary call of ServerPlayer::clearVanishEnchantedItems and Player::dropEquipment
         return CallServerClassMethod<void>("?die@Player@@UEAAXAEBVActorDamageSource@@@Z", player, source);
     }
     original(player, source);
